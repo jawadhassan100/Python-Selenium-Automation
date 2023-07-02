ea110499bc8330198adeaf0e47745e3a1f397558
Jawad Hassan <jawadhassankhan2001@gmail.com>
2023-07-02 17:03:35 +0500

    adding comments

--- ytselenium.py
+++ ytselenium.py
@@ -2,16 +2,32 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import time
+# Setup your browser
 driver = webdriver.Chrome()
+
+# Opens youtube
 driver.get("https://www.youtube.com/")
+
+# Wait for the page to load
 time.sleep(2)
+
+# Search for the video
 elem = driver.find_element(By.NAME, "search_query")
 elem.send_keys("ncs background music")
 elem.send_keys(Keys.RETURN)
+
+# Wait for the page to load
 time.sleep(2)
+
+# Click on the first video
 video = driver.find_element(By.XPATH ,'//*[@id="video-title"]/yt-formatted-string')
 video.click()
+
+# Wait for the page to load
 time.sleep(5)
+
+
+# Get the video duration
 duration_element = driver.find_element(By.XPATH, '//span[@class="ytp-time-duration"]')
 duration_text = duration_element.get_attribute("innerHTML")
 duration_parts = duration_text.split(":")
@@ -19,8 +35,7 @@ duration_seconds = int(duration_parts[-1]) + int(duration_parts[-2]) * 60
 
 # Wait for the video to finish playing
 time.sleep(duration_seconds)
-# play_button = driver.find_element(By.XPATH ,'//*[@id="movie_player"]/div[29]/div[2]/div[1]/button')
-# play_button.click()
-# time.sleep(60)
+
+# Close the browser
 driver.quit()
 
